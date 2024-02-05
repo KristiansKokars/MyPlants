67057a3f72757f4b97ce76dd4f877fd3aef02e31
KristiansKokars <kristians_kokars@outlook.com>
2024-02-05 17:20:42 +0200

    chore: Init project with fonts and colors from design

---
+++ app/src/main/java/com/kristianskokars/myplants/ui/theme/Color.kt
@@ -0,0 +1,36 @@
+package com.kristianskokars.myplants.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Neutralus900 = Color(0xFF232926)
+val Neutralus500 = Color(0xFF516370)
+val Neutralus300 = Color(0xFFAFB3B7)
+val Neutralus100 = Color(0xFFF9F9F9)
+
+val Accent600 = Color(0xFF085D6F)
+val Accent500 = Color(0xFF0A6375)
+val Accent100 = Color(0xFF8CB6B2)
+val Accent0 = Color(0xFFFFFFFF)
+
+val OtherGreen500 = Color(0xFFCFE3C4)
+val OtherGreen100 = Color(0xFFF5F9F3)
+
+val OtherPink500 = Color(0xFFFAD2E2)
+val OtherPink100 = Color(0xFFFFFAFC)
+
+val OtherLime500 = Color(0xFFB2D99E)
+val OtherLime100 = Color(0xFFF9FBF8)
+
+val OtherBlue500 = Color(0xFFA7D1E2)
+val OtherBlue100 = Color(0xFFF5FAFC)
+
+val OtherYellow500 = Color(0xFFDBE1BB)
+val OtherYellow100 = Color(0xFFFDFFF1)
---
+++ app/src/main/java/com/kristianskokars/myplants/ui/theme/Type.kt
@@ -0,0 +1,71 @@
+package com.kristianskokars.myplants.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.unit.sp
+import com.kristianskokars.myplants.R
+import androidx.compose.ui.text.font.Font
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val poppinsFontName = GoogleFont("Poppins")
+val Poppins = FontFamily(
+    Font(googleFont = poppinsFontName, fontProvider = provider),
+    Font(googleFont = poppinsFontName, fontProvider = provider, weight = FontWeight.Bold),
+    Font(googleFont = poppinsFontName, fontProvider = provider, weight = FontWeight.SemiBold),
+    Font(googleFont = poppinsFontName, fontProvider = provider, weight = FontWeight.Light),
+    Font(R.font.poppins_regular)
+)
+
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        color = Neutralus900
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        color = Neutralus500
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        color = Neutralus500
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        color = Neutralus500
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        color = Neutralus500
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
+        lineHeight = 16.sp,
+        color = Neutralus500
+    ),
+)
