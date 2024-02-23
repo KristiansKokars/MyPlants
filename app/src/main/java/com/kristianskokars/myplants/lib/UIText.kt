2df53f73f0bcc83db4c6935cb8d6f5a878ba5b37
KristiansKokars <kristians_kokars@outlook.com>
2024-02-23 20:46:16 +0200

    feat: added UI text conversion to string in composable context

--- app/src/main/java/com/kristianskokars/myplants/lib/UIText.kt
+++ app/src/main/java/com/kristianskokars/myplants/lib/UIText.kt
@@ -2,11 +2,18 @@ package com.kristianskokars.myplants.lib
 
 import android.content.Context
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 sealed class UIText {
     data class StringResource(@StringRes val id: Int) : UIText()
 
     fun get(context: Context) = when (this) {
         is StringResource -> context.getString(id)
+    }
+
+    @Composable
+    fun toUIString() = when (this) {
+        is StringResource -> stringResource(id = id)
     }
 }
