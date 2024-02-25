663f28f839491a25c1e3e1bc11249379bbf47214
KristiansKokars <kristians_kokars@outlook.com>
2024-02-25 14:26:50 +0200

    fix: removed safe content padding and replaced with hardcoded padding, as devices would have unexpected behaviour with it

--- app/src/main/java/com/kristianskokars/myplants/core/presentation/components/ScreenSurface.kt
+++ app/src/main/java/com/kristianskokars/myplants/core/presentation/components/ScreenSurface.kt
@@ -2,12 +2,13 @@ package com.kristianskokars.myplants.core.presentation.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 
 @Composable
@@ -26,7 +27,7 @@ fun ScreenSurface(
             Box(modifier = Modifier.zIndex(-10f)) {
                 backgroundContent()
             }
-            Box(modifier = if (shouldPadAgainstInsets) Modifier.safeContentPadding() else Modifier) {
+            Box(modifier = if (shouldPadAgainstInsets) Modifier.padding(top = 12.dp) else Modifier) {
                 content()
             }
         }
