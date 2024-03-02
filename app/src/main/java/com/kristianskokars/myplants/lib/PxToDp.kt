11c46fe33d90eb4d4b33dc756adc6f7f01e0f280
KristiansKokars <kristians_kokars@outlook.com>
2024-03-02 18:41:37 +0200

    refactor: removed unused lib method

--- app/src/main/java/com/kristianskokars/myplants/lib/PxToDp.kt
+++
@@ -1,7 +0,0 @@
-package com.kristianskokars.myplants.lib
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalDensity
-
-@Composable
-fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
